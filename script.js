e649ddef12873325fc6c0f8431667fd26e1b2d4e
Avi Adler <102553438+aviadlers@users.noreply.github.com>
2025-08-05 15:51:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ setInterval(rotateSubtitles, 3000);
 
 // Image Slideshow
 const images = [
-  "images/2F618B96-095B-44CD-A710-4C383EDB0B19", "images/IMG_3686", "images/IMG_3687",
+  "images/2F618B96-095B-44CD-A710-4C383EDB0B19.png", "images/IMG_3686", "images/IMG_3687",
   "images/0F59827B-F82F-405A-AF1D-B2B0AA0E499C", "images/4F533DA2-5AD6-400D-8482-556282ED7C1A", "images/17873287-BA51-47F4-9E8B-417CA09D5D21",
   "images/E278C109-CD86-4A75-AD01-CA49588AB2EB", "images/E88B597B-BB68-4309-8EB7-78503F20AEC3", "images/Thank you"
 ];
